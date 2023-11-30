297c3bef9a5bbbcb91f2b63c42f54574e8a83ea4
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-11-30 13:38:38 +0100

    Header with placeholders, changed filestructure

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="normalize.css" rel="stylesheet">
-        <link href="style.css" rel="stylesheet">
+        <link href="css/normalize.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
         <title>Weatherstation</title>
     </head>
     <body>
-
+        <header>
+            <div>
+                <h1>Weatherstation</h1>
+                <h2>Last update: {Date}, {Time}</h2>
+            </div>
+        </header>
     </body>
 </html>
