8452f901a0c675d882effba56a20349369df8e7c
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-11-30 15:43:24 +0100

    itemblocks, css and icons, added placeholder items in main

--- index.php
+++ index.php
@@ -14,5 +14,43 @@
                 <h2>Last update: {Date}, {Time}</h2>
             </div>
         </header>
+        <main>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/thermometer.svg">
+                <p class="itemname">Temperature</p>
+                <p class="itemvalue">10&deg;C</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+        </main>
     </body>
 </html>
