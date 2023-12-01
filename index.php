4de922a2c93e3b9e72aeb27f9c3fc9940c2f8581
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-12-01 14:25:31 +0100

    wind, humidity, co2 icon and placeholders

--- index.php
+++ index.php
@@ -22,9 +22,15 @@
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
-                <img class="icon" src="img/thermometer.svg">
-                <p class="itemname">Temperature</p>
-                <p class="itemvalue">10&deg;C</p>
+                <img class="icon" src="img/strong-wind.svg">
+                <p class="itemname">Wind speed</p>
+                <p class="itemvalue">4 km/h</p>
+                <img class="more" src="img/nav-arrow-right.svg">
+            </div>
+            <div class="item">
+                <img class="icon" src="img/humidity.svg">
+                <p class="itemname">Humidity</p>
+                <p class="itemvalue">89%</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
@@ -34,15 +40,9 @@
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
-                <img class="icon" src="img/thermometer.svg">
-                <p class="itemname">Temperature</p>
-                <p class="itemvalue">10&deg;C</p>
-                <img class="more" src="img/nav-arrow-right.svg">
-            </div>
-            <div class="item">
-                <img class="icon" src="img/thermometer.svg">
-                <p class="itemname">Temperature</p>
-                <p class="itemvalue">10&deg;C</p>
+                <img class="icon" src="img/cloud.svg">
+                <p class="itemname">CO2</p>
+                <p class="itemvalue">421 PPM</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
