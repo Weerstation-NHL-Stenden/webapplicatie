708b2e11763a3c92db4293c72fc4b5609e02a0cd
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-12-04 12:22:07 +0100

    Replaced placeholders

--- index.php
+++ index.php
@@ -34,9 +34,9 @@
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
-                <img class="icon" src="img/thermometer.svg">
-                <p class="itemname">Temperature</p>
-                <p class="itemvalue">10&deg;C</p>
+                <img class="icon" src="img/">
+                <p class="itemname">Wind direction</p>
+                <p class="itemvalue">West</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
@@ -46,9 +46,9 @@
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
-                <img class="icon" src="img/thermometer.svg">
-                <p class="itemname">Temperature</p>
-                <p class="itemvalue">10&deg;C</p>
+                <img class="icon" src="img/rain.svg">
+                <p class="itemname">Rain</p>
+                <p class="itemvalue">2 mm/h</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
         </main>
