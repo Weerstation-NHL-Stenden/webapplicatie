12d57bd60313fac7cdcc302b2e1ac1543351ae94
Kjeld-H <kjeld.heemskerk@student.nhlstenden.com>
2023-12-11 10:32:33 +0100

    echo value

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ getLastReadings();
         <header>
             <div>
                 <h1>Weatherstation</h1>
-                <h2>Last update: {Date}, {Time}</h2>
+                <h2>Last update: <?=$result[7]?></h2>
             </div>
         </header>
         <main>
@@ -28,13 +28,13 @@ getLastReadings();
             <div class="item">
                 <img class="icon" src="img/strong-wind.svg">
                 <p class="itemname">Wind speed</p>
-                <p class="itemvalue">4 km/h</p>
+                <p class="itemvalue"><?=$result[1]?> km/h</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
                 <img class="icon" src="img/humidity.svg">
                 <p class="itemname">Humidity</p>
-                <p class="itemvalue">89%</p>
+                <p class="itemvalue"><?=$result[2]?>%</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
@@ -46,7 +46,7 @@ getLastReadings();
             <div class="item">
                 <img class="icon" src="img/cloud.svg">
                 <p class="itemname">CO2</p>
-                <p class="itemvalue">421 PPM</p>
+                <p class="itemvalue"><?=$result[4]?> PPM</p>
                 <img class="more" src="img/nav-arrow-right.svg">
             </div>
             <div class="item">
